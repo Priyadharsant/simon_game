115465f1de9e8c6aecffd2fa3415343b8e9b3e7e
Priyadharsan T <priyadharsant4@gmail.com>
2025-09-23 22:42:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,98 @@
+let game = "off";
+let level = 1;
+let colors = ['green','red','blue','yellow'];
+let random = [];   
+let click = 0;     
+let btn = $("button"); 
+let level_lbl = $(".level");
+let max_level = $(".max-level")
+let alert_msg = $(".on_alert")
+let game_over = $('.game_over')
+
+function simon() {
+    console.log('Level : ' + level);
+    
+    level_lbl.text("Level : "+level)
+    
+    random.push(colors[Math.floor(Math.random() * 4)]);
+    console.log("Sequence: " + random.join(", "));
+
+    
+    for (let i = 0; i < random.length; i++) {
+        setTimeout(() => {
+            let $btn = $('#' + random[i]);
+            $btn.addClass('blink');
+
+            setTimeout(() => {
+                $btn.removeClass('blink');
+            }, 300);
+
+        }, i * 1000); 
+    }
+
+    if (level > localStorage.getItem('max_level')) {
+        max_level.text('Max-level : ' + level)
+        localStorage.setItem('max_level', level)
+    }
+    level++;
+    click = 0;
+}
+
+$(".btn").click(function() {
+    let $click = $(this)
+    let chosen = $click.attr("id"); 
+    
+    if(game === 'off')
+    {
+        alert_msg.show();   
+    }
+    else{
+        alert_msg.hide()
+    }
+    
+    
+    if (chosen === random[click]) {
+        click++;
+
+       
+        if (click === random.length) {
+            setTimeout(simon, 300);
+        }
+    } else {
+        console.log("Wrong! Game Over");
+        level_lbl.text("Level : 0")
+        game = "off";
+        level = 1;
+        random = [];
+        game_over.show(); 
+        btn.removeClass("btn-inactive") 
+        setTimeout(() => {
+                game_over.hide(); 
+            },1500);
+        
+        start();
+    }
+});
+alert_msg.hide()
+game_over.hide()
+function start(){
+    if(localStorage.getItem('max_level') === null)
+    {
+        localStorage.setItem('max_level', 0)
+    }
+    else{
+        max_level.text('Max-level : ' + localStorage.getItem('max_level'))
+    }
+    btn.click(function () {
+        if (game === 'off') {
+            alert_msg.hide()
+            game = 'on';
+            btn.addClass("btn-inactive")
+            game_over.hide()
+            simon();
+        }
+    });
+}
+
+ 
+start();
